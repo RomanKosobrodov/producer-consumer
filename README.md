2c80c1b7b38c7692a15af9cf25d54d51f86e470e
Roman <roman@kosobrodov.net>
2024-07-30 09:59:52 +1000

    updated output formatting

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-# producer-consumer
+# Benchmarks of Single Producer Multiple Consumer implementations
 
 Comparison between several implementations of data exchange between a single producer and multiple consumers.
 
 # Disclaimer
 
-This code is written for educational and entertainment purposes. It is not meant for production.
+This code is written for educational purposes. It is not meant for production.
 
-# Motivation
+# Overview
+
+A Single Producer Multiple Consumer (SPMC) design is often used in audio processing and other applications that process a stream of sensor data in soft real time systems. The performance of SPMC data structures is usually defined by the mechanism used to synchronize data between the producer and consumer threads. This repository compares several implementations of SPMC:
+
+- _Baseline_ solution measures the time required to write and read data without any synchronization. To avoid undefined behaviour two memory buffers are used: one for reading and one for writing.
+
+- _Exclusive locks_ using `std::mutex`. This is a practical, but suboptimal solution as only one thread has access to data at any given time. The producer (writer) and consumers (readers) have equal priority at obtaining the mutex.
+- _Shared locks_ using `std::shared_mutex` which is an improvement of the previous solution. The writer has exclusive access to the memory while multiple consumers share the lock which enables concurrent read access.
+- _SeqLocks_, a lock-free solution commonly used in financial applications. Synchronization is achieved with atomic counters. There is no blocking of the producer (writer) thread, while the readers check if the data is being written and retry if this is the case. It should be noted that this mechanism is incomplete and unless the data itself is atomic, race conditions still occur as the producer can potentially write into a section of memory which is being read by a consumer.
+- _ZeroMQ inprocess_ provides an alternative mechanism for exchanging data between several threads.
+
+As an additional optimization, the underlying data structure is implemented as a ring buffer.
+
+# Methodology
+
+Most benchmarks of lock-free data structures focus on the performance of the synchronization mechanism by itself. While this approach is valid for many applications especially in finance, it provides little insight into an overall performance of memory-bound systems where large volumes of data are being exchanged, for instance audio.
+
+In this repository the benchmarks take into account the size of the data and the number of consumers. The length of the ring buffer measured in data blocks is another parameter.
--- src/benchmarks/main.cpp
+++ src/benchmarks/main.cpp
@@ -12,15 +12,85 @@
 #include <mutex>
 #include <format>
 #include <vector>
+#include <algorithm>
 
-void print_results(const std::string &message, const std::vector<double> &times)
+struct parameters
 {
-    fmt::print("\"{}\", {:.1f}", message, times[0]);
-    for (auto k = 1; k < times.size(); ++k)
+    std::size_t num_blocks{10};
+    std::size_t block_size{16};
+    std::size_t num_readers{3};
+    std::size_t num_cycles{1000000};
+};
+
+void print_results(const std::string &message, const parameters &params, std::vector<double> &times, const char separator = ' ')
+{
+    fmt::print("{}", "{\n");
+    fmt::print("\"implementation\": \"{}\",\n", message);
+    fmt::print("\"num_cycles\": \"{}\",\n", params.num_cycles);
+    fmt::print("\"block_size\": \"{}\",\n", params.block_size);
+    fmt::print("\"num_blocks\": \"{}\",\n", params.num_blocks);
+    fmt::print("\"num_readers\": \"{}\",\n", params.num_readers);
+    fmt::print("\"reader\": {:.1f},\n", times[0]);
+    fmt::print("\"writers\": [");
+    std::vector<double> sorted(std::begin(times) + 1, std::end(times));
+    std::sort(std::begin(sorted), std::end(sorted));
+    for (auto k = 0; k < sorted.size() - 1; ++k)
     {
-        fmt::print(", {:.1f}", times[k]);
+        fmt::print("{:.1f}, ", sorted[k]);
     }
-    fmt::print("\n");
+    fmt::print("{:.1f}]\n", sorted[sorted.size() - 1]);
+    fmt::print("{}{}\n", "}", separator);
+}
+
+void run_benchmark(const parameters &p)
+{
+    constexpr std::size_t alignment_bytes{16};
+    using data_type = std::uint64_t;
+
+    std::vector<double> results;
+
+    using memcpy_solution_type = memcpy_solution<data_type, alignment_bytes>;
+    results = run_benchmark<memcpy_solution_type, data_type, alignment_bytes>(p.num_blocks,
+                                                                              p.block_size,
+                                                                              p.num_readers,
+                                                                              p.num_cycles);
+    print_results("Memcpy", p, results, ',');
+
+    using unsync_solution_type = unsync_solution<data_type, alignment_bytes>;
+    results = run_benchmark<unsync_solution_type, data_type, alignment_bytes>(p.num_blocks,
+                                                                              p.block_size,
+                                                                              p.num_readers,
+                                                                              p.num_cycles);
+    print_results("Unsync", p, results, ',');
+
+    using seqlock_solution_type = seqlock_solution<data_type, alignment_bytes>;
+    results = run_benchmark<seqlock_solution_type, data_type, alignment_bytes>(p.num_blocks,
+                                                                               p.block_size,
+                                                                               p.num_readers,
+                                                                               p.num_cycles);
+    print_results("SeqLock", p, results, ',');
+
+    using shared_solution_type = shared_solution<data_type, alignment_bytes>;
+    results = run_benchmark<shared_solution_type, data_type, alignment_bytes>(p.num_blocks,
+                                                                              p.block_size,
+                                                                              p.num_readers,
+                                                                              p.num_cycles);
+    print_results("Shared mutex", p, results, ',');
+
+    using exclusive_solution_type = exclusive_solution<data_type, alignment_bytes>;
+    results = run_benchmark<exclusive_solution_type, data_type, alignment_bytes>(p.num_blocks,
+                                                                                 p.block_size,
+                                                                                 p.num_readers,
+                                                                                 p.num_cycles);
+    print_results("Mutex", p, results, ',');
+
+    parameters p_zmq = p;
+    p_zmq.num_cycles = 100;
+    results = run_zmq_benchmark<data_type, alignment_bytes>(p_zmq.block_size,
+                                                            p_zmq.num_readers,
+                                                            p_zmq.num_cycles);
+
+    print_results("ZMQ", p, results);
 }
 
 int main(int argc, char *argv[])
@@ -28,59 +98,10 @@ int main(int argc, char *argv[])
     std::shared_ptr<spdlog::logger> file_logger = spdlog::rotating_logger_mt("benchmark", "benchmark.log", 1048576 * 5, 3);
     file_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e %z] [%-8t] [%-8l] %v");
     file_logger->set_level(spdlog::level::debug);
-
     spdlog::set_default_logger(file_logger);
 
-    constexpr std::size_t num_blocks{10};
-    constexpr std::size_t block_size{16};
-    constexpr std::size_t alignment_bytes{16};
-    constexpr std::size_t num_readers{3};
-    constexpr std::size_t num_cycles{1000000};
-
-    using data_type = std::uint64_t;
-    std::vector<double> results;
-
-    using memcpy_solution_type = memcpy_solution<data_type, alignment_bytes>;
-    results = run_benchmark<memcpy_solution_type, data_type, alignment_bytes>(num_blocks,
-                                                                              block_size,
-                                                                              num_readers,
-                                                                              num_cycles);
-    print_results("Memcpy", results);
-
-    using unsync_solution_type = unsync_solution<data_type, alignment_bytes>;
-    results = run_benchmark<unsync_solution_type, data_type, alignment_bytes>(num_blocks,
-                                                                              block_size,
-                                                                              num_readers,
-                                                                              num_cycles);
-    print_results("Unsync", results);
-
-    using seqlock_solution_type = seqlock_solution<data_type, alignment_bytes>;
-    results = run_benchmark<seqlock_solution_type, data_type, alignment_bytes>(num_blocks,
-                                                                               block_size,
-                                                                               num_readers,
-                                                                               num_cycles);
-    print_results("SeqLock", results);
-
-    using shared_solution_type = shared_solution<data_type, alignment_bytes>;
-    results = run_benchmark<shared_solution_type, data_type, alignment_bytes>(num_blocks,
-                                                                              block_size,
-                                                                              num_readers,
-                                                                              num_cycles);
-    print_results("Shared mutex", results);
-
-    using exclusive_solution_type = exclusive_solution<data_type, alignment_bytes>;
-    results = run_benchmark<exclusive_solution_type, data_type, alignment_bytes>(num_blocks,
-                                                                                 block_size,
-                                                                                 num_readers,
-                                                                                 num_cycles);
-    print_results("Mutex", results);
-
-    constexpr std::size_t num_zmq_cycles{100};
-    results = run_zmq_benchmark<data_type, alignment_bytes>(block_size,
-                                                            num_readers,
-                                                            num_zmq_cycles);
-
-    print_results("ZMQ", results);
+    parameters p;
+    run_benchmark(p);
 
     file_logger->flush();
     spdlog::shutdown();
